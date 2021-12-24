e851127b7f922d6a4d442f105aa0ac1fc797177d
b10t <b10t.rus@gmail.com>
2021-12-24 09:37:34 +0800

    Переделал модели и сделал миграции.

--- bot/models.py
+++ bot/models.py
@@ -1,36 +1,43 @@
 from django.db import models
 
 
+class Player(models.Model):
+    firs_name = models.CharField('Имя', max_length=250)
+    last_name = models.CharField('Фамилия', max_length=250, blank=True)
+    chat_id = models.IntegerField('Chat id', unique=True)
+    phone = models.CharField('Номер телефона', max_length=20, unique=True)
+    mail = models.CharField('Электронная почта', max_length=250, blank=True)
+
+    def __str__(self):
+        return f'{self.firs_name} {self.last_name} {self.phone}'
+
+    class Meta:
+        verbose_name = 'Игрок'
+        verbose_name_plural = 'Игроки'
+
+
 class Game(models.Model):
     title = models.CharField('Название игры', max_length=250)
     owner = models.ForeignKey(
         Player,
         on_delete=models.SET_NULL,
+        null=True,
         verbose_name='Владелец игры',
-        related_name='players'
+        related_name='owner'
     )
-    cash_limit = models.CharField('Бюджет', default='0')
+    cash_limit = models.CharField('Бюджет', default='0', max_length=25)
     stop_registration_date = models.DateTimeField('Окончание регистрации')
     sending_gift_date = models.DateField('Дата отправки подарков')
+    is_finish = models.BooleanField(default=False, blank=False,
+                                    db_index=True,
+                                    verbose_name='Игра закончена')
 
     def __str__(self):
         return f'Игра {self.pk} - {self.title}, владелец {self.owner}'
 
-
-class Player(models.Model):
-    firs_name = models.CharField('Имя', max_length=250)
-    last_name = models.CharField('Фамилия', max_length=250, blank=True)
-    vk_id = models.CharField('Vk id', max_length=50, unique=True)
-    phone = models.CharField('Номер телефона', max_length=20, unique=True)
-    mail = models.CharField('Электронная почта', max_length=250, blank=True)
-    games = models.ManyToManyField(
-        Game,
-        verbose_name='Участвует в играх',
-        related_name='players_in_game'
-    )
-
-    def __str__(self):
-        return f'{self.firs_name} {self.last_name} {self.phone}'
+    class Meta:
+        verbose_name = 'Игра'
+        verbose_name_plural = 'Игры'
 
 
 class PlayersInGame(models.Model):
@@ -38,13 +45,13 @@ class PlayersInGame(models.Model):
         Game,
         on_delete=models.CASCADE,
         verbose_name='Номер игры',
-        related_name='games',
+        related_name='game',
     )
     player = models.ForeignKey(
         Player,
         on_delete=models.CASCADE,
         verbose_name='Участник',
-        related_name='players',
+        related_name='player',
     )
     santa = models.ForeignKey(
         Player,
@@ -52,8 +59,12 @@ class PlayersInGame(models.Model):
         blank=True,
         on_delete=models.SET_NULL,
         verbose_name='Тайный Санта',
-        related_name='players',
+        related_name='santa',
     )
 
     def __str__(self):
         return f'Игра {self.game.pk}, игрок {self.player}, его Санта {self.santa}'
+
+    class Meta:
+        verbose_name = 'Игрок в игре'
+        verbose_name_plural = 'Игроки в игре'
